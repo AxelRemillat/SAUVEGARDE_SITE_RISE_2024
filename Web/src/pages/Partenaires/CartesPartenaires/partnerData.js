937c619025be064b78bf8066c937c25ad0017488
MathisLevrot2 <levmp002@mymail.unisa.edu.au>
2026-01-16 09:54:48 +0100

    dernieres modifs avant d'integrer codex

--- Web/src/pages/Partenaires/CartesPartenaires/partnerData.js
+++ Web/src/pages/Partenaires/CartesPartenaires/partnerData.js
@@ -7,17 +7,17 @@ const partnerData = {
     imgSrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Esme-sudria-logo.png/1024px-Esme-sudria-logo.png"
   },
   partner2: {
-    title: "Ecole agricole",
+    title: "exemple sup bio",
     description: "Soutien logistique et visibilité innovation.",
     imgSrc: "https://yt3.googleusercontent.com/tuXIFeLdc9MvtyzqBuwmWVNJztfgbIdA7j7PE1dZnU2zsrkPLo7eo0quMwL7NbFahUfwaAV7iQM=s900-c-k-c0x00ffffff-no-rj"
   },
   partner3: {
-    title: "Revolut",
+    title: "exemple Revolut",
     description: "Collaboration internationale pour projets étudiants.",
     imgSrc: "https://fintechboostup.com/wp-content/uploads/2024/07/Revolut-logo.png"
   },
   partner4: {
-    title: "Avant dernier au classement",
+    title: "exemple ISG",
     description: "Soutien développement durable et entrepreneuriat social.",
     imgSrc: "https://newsroom.ionis-group.com/wp-content/uploads/2019/09/LOGO_ISG_QUADRI.png"
   }
