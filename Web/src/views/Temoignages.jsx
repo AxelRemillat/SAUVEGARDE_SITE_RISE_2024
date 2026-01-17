fa8e41eb1fada6e9ced7c643a389531871b45f70
MathisLevrot2 <levmp002@mymail.unisa.edu.au>
2026-01-17 21:25:50 +0100

    netoyage j'ai retiré un titre

--- Web/src/views/Temoignages.jsx
+++ Web/src/views/Temoignages.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from "../components/StylePages/Banner";
 import Temoignages from '../components/Temoignages/temoignagestempo'; // Le composant carrousel
-import TitreHistoires from '../pages/Temoignages/ElementsStyle/TitreHistoires';
+
 import TexteTemoignages from '../pages/Temoignages/ElementsStyle/BloqueTexte';
 
 
@@ -9,7 +9,6 @@ const PageTemoignages = () => {
   return (
     <div>
       <Banner text="Découvrez leurs aventures" />
-      <TitreHistoires />
       <TexteTemoignages />
       <Temoignages />
     </div>
